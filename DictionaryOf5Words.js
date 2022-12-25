99ac45ab88a33b66a71ff4a991d479417ef7aa79
Pankaj <rcypankaj@gmail.com>
2022-12-25 23:00:01 +0530

    Initial Commit

---
+++ DictionaryOf5Words.js
@@ -0,0 +1,14 @@
+/* Write a js program to create a word-meaning dictionary of 5 Words.*/
+// Start
+const dict = {
+    Advice : "an opinion that someone offers you about what you should do or how you should act in a particular situation:",
+    Advertisement : "a picture, short film, song, etc. that tries to persuade people to buy a product or service:",
+    Leverage : "the action or advantage of using a lever:",
+    Content : "pleased with your situation and not hoping for change or improvement:",
+    Opportunity : "an occasion or situation that makes it possible to do something that you want to do or have to do, or the possibility of doing something:"
+  }
+  
+  console.log(dict.Opportunity);
+  console.log(dict['Leverage']);
+  console.log(dict);
+  // End
