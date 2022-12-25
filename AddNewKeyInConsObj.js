73f3decae58fed68cb67c963d3e83711eceb3dcb
Pankaj <rcypankaj@gmail.com>
2022-12-25 22:39:18 +0530

    Initial Commit

---
+++ AddNewKeyInConsObj.js
@@ -0,0 +1,16 @@
+
+/* Add new key in to const obj in javaScript.*/
+// Start
+
+const a = {
+    name: "Pankaj Yadav",
+    rollNo: 42,
+    course: "MCA"
+}
+console.log(a);
+
+a['name'] = "Sujal Yadav";
+a['section'] = 18;
+console.log(a);
+
+// End
