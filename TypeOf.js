d51101238c23846cc1635a0ff10e25d6610e494c
Pankaj <rcypankaj@gmail.com>
2022-12-25 18:44:42 +0530

    Initial Commit

---
+++ TypeOf.js
@@ -0,0 +1,9 @@
+
+/* Use type of operator to find the datatype of the string in last question.*/
+// Start
+console.log("Type of Value");
+let a = "Pankaj Yadav";
+let b = 15;
+console.log(typeof (a+b));
+
+// End
