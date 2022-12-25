a01baa8c2eb04551d501606fa9666f60b22bb948
Pankaj <rcypankaj@gmail.com>
2022-12-25 22:17:33 +0530

    Initial Commit

---
+++ consObj.js
@@ -0,0 +1,11 @@
+/* Ceate a const Object in javaScript*/
+// Start
+console.log("ConstObject");
+const a = {
+  name : "Pankaj Yadav",
+  section : 1,
+  isPrinciple : false
+}
+console.log(a);
+
+// End
