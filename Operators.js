ea0c297d0bddfb7284c5682650016b1142212448
Pankaj <rcypankaj@gmail.com>
2022-12-25 23:55:51 +0530

    Initial Commit

---
+++ Operators.js
@@ -0,0 +1,51 @@
+// Operators in js.
+// Start
+console.log("Arithmetic Operators in js");
+let a = 2;
+let b = 4;
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b = ", a/b);
+console.log("a*b = ", a*b);
+console.log("a**b = ", a**b);
+console.log("a++ = ", a++);
+console.log("a-- = ", a--);
+console.log("++a = ", ++a);
+console.log("--a = ", --a);
+
+console.log();
+console.log("Assignment Operators in js");
+let assignment = 2;
+assignment += 7;
+console.log("assignment is now", assignment);
+assignment -= 2;
+console.log("Assignment is now", assignment);
+assignment *= 2;
+console.log("Assignment is now", assignment);
+assignment /= 2;
+console.log("Assignment is now", assignment);
+
+console.log();
+console.log("Comparison Operators in js");
+let comp1 = 12 comp2 = 7;
+console.log("comp1 == comp2 is ", comp1 == comp2);
+console.log("comp1 != comp2 is ", comp1 != comp2);
+console.log("comp1 === comp2 is ", comp1 === comp2);
+console.log("comp1 !== comp2 is ", comp1 !== comp2);
+console.log("comp1 > comp2 is ", comp1 > comp2);
+
+console.log();
+console.log("Logical Operators in js");
+let x = 10, y = 15;
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!false);
+console.log(!true);
+
+// Comments
+// This is a single line comment
+/* This is a sample multiline
+comment */
+
+// End
