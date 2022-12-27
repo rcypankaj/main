546345a2578b9f9ba63fa7d604f2050c024b19b9
Pankaj <rcypankaj@gmail.com>
2022-12-27 12:04:25 +0530

    Initial Commit

---
+++ Strings.js
@@ -0,0 +1,22 @@
+/* Strings in JavaScript */
+
+// Start
+
+// length of the string
+let name = 'Pankaj Yadav';
+console.log(name.length);
+
+console.log(name[0]);
+console.log(name[1]);
+
+// Template Literal
+let friend1 = "Pankaj";
+let friend2 = "Yadav";
+let sentence = `${friend1} and ${friend2} my friend`;
+console.log(sentence);
+
+// Escape Sequence Character
+name = "S\tuj\"a\'l";
+console.log(name);
+
+// End
