8bf80678dfe432f4acaa78478f8d11ae82fad798
Pankaj <rcypankaj@gmail.com>
2022-12-27 13:51:59 +0530

    Initial Commit

---
+++ StringsMethod.js
@@ -0,0 +1,40 @@
+/* Strings Method in JavaScript */
+
+// Start
+
+// length of the string
+let name = '   Pankaj Yadav';
+console.log(name.length);
+
+// for uppercase
+console.log(name.toUpperCase());
+
+// for LowerCase
+console.log(name.toLowerCase());
+
+// for Slice
+console.log(name.slice(0,6));
+
+// for replace
+console.log(name.replace("Yadav", "Kumar"));
+
+// for concatenate
+console.log(name.slice(0,6).concat(" Kumar Yadav"));
+
+console.log(name);
+// for space remove
+console.log(name.trim());
+
+
+let fr = "Raman" + "Shivika" + "Harry";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[4]);
+console.log(fr[5]);
+console.log(fr[6]);
+console.log(fr[7]);
+
+console.log(fr);
+
+// End
