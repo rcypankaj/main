8ed5bdb9e586a5c15da2ef85c2d8b7c62ac3498f
Pankaj <rcypankaj@gmail.com>
2022-12-27 14:36:11 +0530

    Initial Commit

---
+++ Arrays.js
@@ -0,0 +1,22 @@
+/* Arrays in JavaScript */
+
+// Start
+
+let marks_pankaj = [91, 82, 63, 84, false, "Not Present"];
+console.log(marks_pankaj[0]);
+console.log(marks_pankaj[1]);
+console.log(marks_pankaj[2]);
+console.log(marks_pankaj[3]);
+console.log(marks_pankaj[4]);
+console.log(marks_pankaj[5]);
+console.log(marks_pankaj[6]);
+console.log();
+console.log(marks_pankaj);
+console.log("The length of marks_pankaj is", marks_pankaj.length);
+marks_pankaj[6] = 89; // Adding a new value to the array
+marks_pankaj[0] = 96; // Changing the value of an array
+
+console.log(marks_pankaj);
+console.log(typeof marks_pankaj);
+
+// End
