7c53ccb79fe56f7135bae4e310f725009b97f93c
Pankaj <rcypankaj@gmail.com>
2022-12-28 12:48:56 +0530

    Initial Commit

---
+++ SquareOfAnArray.js
@@ -0,0 +1,13 @@
+/* Create an array of square of given array */
+
+// Start
+
+let arr = [1, 2, 5, 7, 9, 10, 15, 36, 40];
+let x = arr.map((a) => {
+    return a * a;
+})
+
+console.log(x);
+
+
+// End
