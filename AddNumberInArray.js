d46241fb444e9101e3e20106caae2f5d0c27f2fd
Pankaj <rcypankaj@gmail.com>
2022-12-28 12:31:48 +0530

    Initial Commit

---
+++ AddNumberInArray.js
@@ -0,0 +1,10 @@
+/* Create an array of numbers and take input from the user to add number to this array */
+
+// Start
+let arr = [1, 2, 5, 7, 9, 10, 15, 36, 4];
+let x = prompt("Enter a number");
+x = Number.parseInt(x);
+arr.push(x);
+console.log(arr);
+
+// End
