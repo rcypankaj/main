f81afc3a96f657e59e463263fd1b126157340acc
Pankaj <rcypankaj@gmail.com>
2022-12-29 13:54:43 +0530

    Initial Commit

---
+++ ChangeBackgroundColorUserChoice.js
@@ -0,0 +1,9 @@
+/* Change the background color of the user choice when he enter the color name 4*/
+
+// Start
+let color = prompt("Enter a color");
+
+document.body.style.background = color;
+
+
+// End
