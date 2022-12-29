5b4bd9d5373e2aa30d3c7937fed8dca10c7fd3ad
Pankaj <rcypankaj@gmail.com>
2022-12-29 13:43:42 +0530

    Initial Commit

---
+++ CanDrive1.js
@@ -0,0 +1,31 @@
+/* Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive */
+// Also ask the user does he want to play again
+//if user enter negative value then say to user enter correct age
+
+// Start
+const canDrive = (age) => {
+    return age >= 18 ? true : false;
+}
+
+let playAgain = true;
+do {
+    let age = prompt("Enter your age? ");
+
+    age = Number.parseInt(age);
+    if (age < 0) {
+        alert("Please enter a valid age");
+        continue;
+    }
+
+    if (canDrive(age)) {
+        alert("Yes you can drive");
+    }
+    else {
+        alert("You cannot drive");
+    }
+
+    playAgain = confirm("Do you want to play again");
+} while (playAgain);
+
+
+  // End
